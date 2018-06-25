ffdc1453a7cc279dbc2de7c3c418827a52133639
Jack Case <jackacase@gmail.com>
2018-06-24 21:34:51 -0400

    Testing CSV parsing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 
+#include "csv.h"
 #include "track_point_2d.h"
 #include "track_draw_point_2d.h"
 #include "track_2d.h"
 
 int main() {
+
+    io::CSVReader<2> reader("Test_multi_record.csv");
+    reader.read_header(io::ignore_extra_column, "time", "gFx");
+    std::cout << reader.next_line();
+
+
+    std::ifstream csvfile;
+    csvfile.open("Test_multi_record.csv");
+    std::string line;
+    csvfile >> line;
+    std::cout << line;
+
+    /*
     track_point_2d point1(0, 0);
     track_point_2d point2(20, 80);
     track_point_2d point3(38, 90);
@@ -19,6 +34,7 @@ int main() {
     track_2d coolTrack(track);
     coolTrack.process_track();
     coolTrack.print_track();
+    */
 
     return 0;
 }
